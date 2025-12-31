1e3b91f7f2d9ebd58bbdb8338559d03e1987a34c
Md Abdus Sabur <absabur929@gmail.com>
2026-01-01 00:20:52 +0600

    next.config to _redirect to redirect domain

--- next.config.mjs
+++ next.config.mjs
@@ -8,18 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        // Matches every single path on the old domain
-        source: "/:path*",
-        // Sends them to the exact same path on the new domain
-        destination: "https://sharifosmanhadi.info/:path*",
-        // 308 Permanent Redirect (tells Google the site has moved forever)
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
